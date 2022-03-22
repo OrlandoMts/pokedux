65b45adeca10786e35b7df9e62deb68ed9e4fef6
OrlandoMts <dani_monts19@hotmail.com>
2022-03-21 22:05:37 -0600

    deploy a vercel

--- src/components/Searcher/index.js
+++ src/components/Searcher/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Grid, Search } from 'semantic-ui-react';
 import { filterPokemon } from '../../redux/actions';
